04ee1bd8107b25501111e6aeed40df83614e55af
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-04-27 12:49:57 +0200

    Unit tests for Test-DscParameterState follow style guideline

--- tests/Unit/Public/Test-DscParameterState.Tests.ps1
+++ tests/Unit/Public/Test-DscParameterState.Tests.ps1
@@ -7,11 +7,12 @@ $ProjectName = ((Get-ChildItem -Path $ProjectPath\*\*.psd1).Where{
 Import-Module $ProjectName -Force
 
 InModuleScope $ProjectName {
-
-    Describe 'Test-DscParameterState' -Tag TestDscParameterState {
-        Context -Name 'When passing values' -Fixture {
+    Describe 'Test-DscParameterState' -Tag 'TestDscParameterState' {
+        Context 'When passing values' {
             It 'Should return true for two identical tables' {
-                $mockDesiredValues = @{ Example = 'test' }
+                $mockDesiredValues = @{
+                    Example = 'test'
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockDesiredValues
@@ -22,8 +23,13 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when a value is different for [System.String]' {
-                $mockCurrentValues = @{ Example = [System.String] 'something' }
-                $mockDesiredValues = @{ Example = [System.String] 'test' }
+                $mockCurrentValues = @{
+                    Example = [System.String] 'something'
+                }
+
+                $mockDesiredValues = @{
+                    Example = [System.String] 'test'
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -34,8 +40,13 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when a value is different for [System.Int32]' {
-                $mockCurrentValues = @{ Example = [System.Int32] 1 }
-                $mockDesiredValues = @{ Example = [System.Int32] 2 }
+                $mockCurrentValues = @{
+                    Example = [System.Int32] 1
+                }
+
+                $mockDesiredValues = @{
+                    Example = [System.Int32] 2
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -46,8 +57,13 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when a value is different for [Int16]' {
-                $mockCurrentValues = @{ Example = [System.Int16] 1 }
-                $mockDesiredValues = @{ Example = [System.Int16] 2 }
+                $mockCurrentValues = @{
+                    Example = [System.Int16] 1
+                }
+
+                $mockDesiredValues = @{
+                    Example = [System.Int16] 2
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -58,8 +74,13 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when a value is different for [UInt16]' {
-                $mockCurrentValues = @{ Example = [System.UInt16] 1 }
-                $mockDesiredValues = @{ Example = [System.UInt16] 2 }
+                $mockCurrentValues = @{
+                    Example = [System.UInt16] 1
+                }
+
+                $mockDesiredValues = @{
+                    Example = [System.UInt16] 2
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -70,8 +91,13 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when a value is different for [Boolean]' {
-                $mockCurrentValues = @{ Example = [System.Boolean] $true }
-                $mockDesiredValues = @{ Example = [System.Boolean] $false }
+                $mockCurrentValues = @{
+                    Example = [System.Boolean] $true
+                }
+
+                $mockDesiredValues = @{
+                    Example = [System.Boolean] $false
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -82,8 +108,10 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when a value is missing' {
-                $mockCurrentValues = @{ }
-                $mockDesiredValues = @{ Example = 'test' }
+                $mockCurrentValues = @{}
+                $mockDesiredValues = @{
+                    Example = 'test'
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -94,8 +122,15 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return true when only a specified value matches, but other non-listed values do not' {
-                $mockCurrentValues = @{ Example = 'test'; SecondExample = 'true' }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = 'false'  }
+                $mockCurrentValues = @{
+                    Example = 'test'
+                    SecondExample = 'true'
+                }
+
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = 'false'
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -107,8 +142,15 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when only specified values do not match, but other non-listed values do ' {
-                $mockCurrentValues = @{ Example = 'test'; SecondExample = 'true' }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = 'false'  }
+                $mockCurrentValues = @{
+                    Example = 'test'
+                    SecondExample = 'true'
+                }
+
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = 'false'
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -120,8 +162,11 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when an empty hash table is used in the current values' {
-                $mockCurrentValues = @{ }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = 'false'  }
+                $mockCurrentValues = @{}
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = 'false'
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -131,8 +176,11 @@ InModuleScope $ProjectName {
                 Test-DscParameterState @testParameters | Should -Be $false
             }
 
-            It 'Should return true when evaluating a table against a CimInstance' -skip:(!($isWindows -or $PSEdition -eq 'Desktop')) {
-                $mockCurrentValues = @{ Handle = '0'; ProcessId = '1000'  }
+            It 'Should return true when evaluating a table against a CimInstance' {
+                $mockCurrentValues = @{
+                    Handle = '0'
+                    ProcessId = '1000'
+                }
 
                 $mockWin32ProcessProperties = @{
                     Handle = 0
@@ -157,8 +205,11 @@ InModuleScope $ProjectName {
                 Test-DscParameterState @testParameters | Should -Be $true
             }
 
-            It 'Should return false when evaluating a table against a CimInstance and a value is wrong' -skip:(!($isWindows -or $PSEdition -eq 'Desktop'))  {
-                $mockCurrentValues = @{ Handle = '1'; ProcessId = '1000'  }
+            It 'Should return false when evaluating a table against a CimInstance and a value is wrong' {
+                $mockCurrentValues = @{
+                    Handle = '1'
+                    ProcessId = '1000'
+                }
 
                 $mockWin32ProcessProperties = @{
                     Handle = 0
@@ -184,8 +235,15 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return true when evaluating a hash table containing an array' {
-                $mockCurrentValues = @{ Example = 'test'; SecondExample = @('1','2') }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = @('1','2')  }
+                $mockCurrentValues = @{
+                    Example = 'test'
+                    SecondExample = @('1','2')
+                }
+
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = @('1','2')
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -196,8 +254,15 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when evaluating a hash table containing an array with wrong values' {
-                $mockCurrentValues = @{ Example = 'test'; SecondExample = @('A','B') }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = @('1','2')  }
+                $mockCurrentValues = @{
+                    Example = 'test'
+                    SecondExample = @('A','B')
+                }
+
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = @('1','2')
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -208,8 +273,14 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when evaluating a hash table containing an array, but the CurrentValues are missing an array' {
-                $mockCurrentValues = @{ Example = 'test' }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = @('1','2')  }
+                $mockCurrentValues = @{
+                    Example = 'test'
+                }
+
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = @('1','2')
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -220,8 +291,15 @@ InModuleScope $ProjectName {
             }
 
             It 'Should return false when evaluating a hash table containing an array, but the property i CurrentValues is $null' {
-                $mockCurrentValues = @{ Example = 'test'; SecondExample = $null }
-                $mockDesiredValues = @{ Example = 'test'; SecondExample = @('1','2')  }
+                $mockCurrentValues = @{
+                    Example = 'test'
+                    SecondExample = $null
+                }
+
+                $mockDesiredValues = @{
+                    Example = 'test'
+                    SecondExample = @('1','2')
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -232,9 +310,12 @@ InModuleScope $ProjectName {
             }
         }
 
-        Context -Name 'When passing invalid types for DesiredValues' -Fixture {
+        Context 'When passing invalid types for DesiredValues' {
             It 'Should throw the correct error when DesiredValues is of wrong type' {
-                $mockCurrentValues = @{ Example = 'something' }
+                $mockCurrentValues = @{
+                    Example = 'something'
+                }
+
                 $mockDesiredValues = 'NotHashTable'
 
                 $testParameters = @{
@@ -265,8 +346,13 @@ InModuleScope $ProjectName {
                     }
                 }
 
-                $mockCurrentValues = @{ Example = New-Object -TypeName MockUnknownType }
-                $mockDesiredValues = @{ Example = New-Object -TypeName MockUnknownType }
+                $mockCurrentValues = @{
+                    Example = New-Object -TypeName MockUnknownType
+                }
+
+                $mockDesiredValues = @{
+                    Example = New-Object -TypeName MockUnknownType
+                }
 
                 $testParameters = @{
                     CurrentValues = $mockCurrentValues
@@ -275,13 +361,15 @@ InModuleScope $ProjectName {
 
                 Test-DscParameterState @testParameters | Should -Be $false
 
-                Assert-MockCalled -CommandName Write-Warning -Exactly -Times 1 -Scope It
+                Assert-MockCalled -CommandName Write-Warning -Exactly -Times 1
             }
         }
 
-        Context -Name 'When passing an CimInstance as DesiredValue and ValuesToCheck is $null' {
-            It 'Should throw the correct error' -skip:(!($isWindows -or $PSEdition -eq 'Desktop')) {
-                $mockCurrentValues = @{ Example = 'something' }
+        Context 'When passing an CimInstance as DesiredValue and ValuesToCheck is $null' {
+            It 'Should throw the correct error' {
+                $mockCurrentValues = @{
+                    Example = 'something'
+                }
 
                 $mockWin32ProcessProperties = @{
                     Handle = 0
@@ -310,5 +398,4 @@ InModuleScope $ProjectName {
 
         Assert-VerifiableMock
     }
-
 }
