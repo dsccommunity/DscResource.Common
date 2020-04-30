08aa5cd12e581dce0fb90ddf5b1e07e271fd2ec8
valainisgt <valainisgt@tutanota.com>
2020-04-29 20:58:39 -0400

    Added Assert-IPAddress function to pass test

---
+++ source/Public/Assert-IPAddress.ps1
@@ -0,0 +1,53 @@
+<#
+    .SYNOPSIS
+    Check the Address details are valid and do not conflict with Address family.
+    If any problems are detected an exception will be thrown.
+
+    .PARAMETER AddressFamily
+    IP address family.
+
+    .PARAMETER Address
+    IP Address
+#>
+function Assert-IPAddress
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateSet('IPv4', 'IPv6')]
+        [System.String]
+        $AddressFamily,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Address
+    )
+
+    [System.Net.IPAddress] $ipAddress = $null
+    if (-not ([System.Net.IPAddress]::TryParse($Address, [ref] $ipAddress)))
+    {
+        New-InvalidArgumentException `
+            -Message ($script:localizedData.AddressFormatError -f $Address) `
+            -ArgumentName 'Address'
+    }
+
+    if ($AddressFamily)
+    {
+        if (($ipAddress.AddressFamily -eq [System.Net.Sockets.AddressFamily]::InterNetwork.ToString()) `
+                -and ($AddressFamily -ne 'IPv4'))
+        {
+            New-InvalidArgumentException `
+                -Message ($script:localizedData.AddressIPv4MismatchError -f $Address, $AddressFamily) `
+                -ArgumentName 'AddressFamily'
+        }
+
+        if (($ipAddress.AddressFamily -eq [System.Net.Sockets.AddressFamily]::InterNetworkV6.ToString()) `
+                -and ($AddressFamily -ne 'IPv6'))
+        {
+            New-InvalidArgumentException `
+                -Message ($script:localizedData.AddressIPv6MismatchError -f $Address, $AddressFamily) `
+                -ArgumentName 'AddressFamily'
+        }
+    }
+}
