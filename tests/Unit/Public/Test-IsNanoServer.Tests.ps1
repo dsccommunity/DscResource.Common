b72fde24c9851b02641a38240a9a61e249516286
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-05-29 15:24:57 +0200

    Fix unit test

--- tests/Unit/Public/Test-IsNanoServer.Tests.ps1
+++ tests/Unit/Public/Test-IsNanoServer.Tests.ps1
@@ -1,40 +1,44 @@
-BeforeAll {
-    $script:moduleName = 'DscResource.Common'
+$script:moduleName = 'DscResource.Common'
 
-    #region HEADER
-    Remove-Module -Name $script:moduleName -Force -ErrorAction 'SilentlyContinue'
+#region HEADER
+Remove-Module -Name $script:moduleName -Force -ErrorAction 'SilentlyContinue'
 
-    Get-Module -Name $script:moduleName -ListAvailable |
-        Select-Object -First 1 |
+Get-Module -Name $script:moduleName -ListAvailable |
+    Select-Object -First 1 |
         Import-Module -Force -ErrorAction 'Stop'
-    #endregion HEADER
-}
+#endregion HEADER
 
 Describe 'Test-IsNanoServer' -Tag 'TestIsNanoServer' {
     BeforeAll {
-        function Get-CimInstance
-        {
-            param
-            (
-                [Parameter()]
-                [System.String]
-                $ClassName
-            )
+        $script:moduleName = 'DscResource.Common'
+    }
+
+    InModuleScope $script:moduleName {
+        BeforeAll {
+            function Get-CimInstance
+            {
+                param
+                (
+                    [Parameter()]
+                    [System.String]
+                    $ClassName
+                )
+            }
         }
     }
 
     Context 'When the current computer is a Datacenter Nano server' {
         BeforeAll {
             Mock -CommandName Get-CimInstance `
-            -ModuleName $ProjectName `
-            -MockWith {
-                [PSCustomObject] @{
-                    OperatingSystemSKU = 143
+                -ModuleName $script:moduleName `
+                -MockWith {
+                    [PSCustomObject] @{
+                        OperatingSystemSKU = 143
+                    }
                 }
-            }
         }
 
-        It 'Should retrun true' {
+        It 'Should return true' {
             Test-IsNanoServer -Verbose | Should -BeTrue
         }
     }
@@ -42,7 +46,7 @@ Describe 'Test-IsNanoServer' -Tag 'TestIsNanoServer' {
     Context 'When the current computer is a Standard Nano server' {
         BeforeAll {
             Mock -CommandName Get-CimInstance `
-                -ModuleName $ProjectName `
+                -ModuleName $script:moduleName `
                 -MockWith {
                     [PSCustomObject] @{
                         OperatingSystemSKU = 144
@@ -50,7 +54,7 @@ Describe 'Test-IsNanoServer' -Tag 'TestIsNanoServer' {
                 }
         }
 
-        It 'Should retrun true' {
+        It 'Should return true' {
             Test-IsNanoServer -Verbose | Should -BeTrue
         }
     }
@@ -58,15 +62,15 @@ Describe 'Test-IsNanoServer' -Tag 'TestIsNanoServer' {
     Context 'When the current computer is not a Nano server' {
         BeforeAll {
             Mock -CommandName Get-CimInstance `
-            -ModuleName $ProjectName `
-            -MockWith {
-                [PSCustomObject] @{
-                    OperatingSystemSKU = 1
+                -ModuleName $script:moduleName `
+                -MockWith {
+                    [PSCustomObject] @{
+                        OperatingSystemSKU = 1
+                    }
                 }
-            }
         }
 
-        It 'Should retrun false' {
+        It 'Should return false' {
             Test-IsNanoServer -Verbose | Should -BeFalse
         }
     }
